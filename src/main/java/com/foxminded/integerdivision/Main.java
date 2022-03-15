69fc37dfd960cb127adec50f7049ed0e762047bc
Volodymyr Samoilenko <vssamoilenko@gmail.com>
2022-03-15 15:27:13 +0200

    first commit

--- src/main/java/com/foxminded/integerdivision/Main.java
+++ src/main/java/com/foxminded/integerdivision/Main.java
@@ -1,6 +1,7 @@
 package com.foxminded.integerdivision;
 
 public class Main {
+
     public static void main(String[] args) {
         Division ResultDivision = new Division();
         ResultDivision.divide(1000, 8);
